74d479bb4332a0d5178b69ed5ca8740f435a4c97
Marcus Vinicius <marcusvinicius23415@gmail.com>
2024-06-15 14:36:40 -0300

    primeiro commit

---
+++ atividade_1/vn.py
@@ -0,0 +1,26 @@
+from coppeliasim_zmqremoteapi_client import*
+import numpy as np
+
+
+client = RemoteAPIClient()
+sim = client.require('sim')
+
+handle_robo = sim.getObject("/PioneerP3DX")
+
+left_robo =  sim.getObject("/PioneerP3DX/leftMotor")
+right_robo =  sim.getObject("/PioneerP3DX/rightMotor")
+wr = np.deg2rad(30)
+wl = np.deg2rad(30)
+
+vel_left = sim.setJointTargetVelocity(left_robo, wl)
+vel_right = sim.setJointTargetVelocity(right_robo, wr)
+sim.startSimulation()
+while(t:=sim.getSimulationTime()) < 3:
+    pos = sim.getObjectPosition(handle_robo, -1)
+    orient = sim.getObjectOrientation(handle_robo, -1)
+    print(pos)
+    print(orient)
+sim.stopSimulation()
+print(handle_robo)
+print(right_robo)
+print(left_robo)
